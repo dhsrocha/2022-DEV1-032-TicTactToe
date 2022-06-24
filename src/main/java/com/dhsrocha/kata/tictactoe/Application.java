6a7fd77472dfc97223d2ec6c062262cf8cefd6cc
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2022-06-24 14:26:30 +0200

    Project scaffold.

---
+++ src/main/java/com/dhsrocha/kata/tictactoe/Application.java
@@ -0,0 +1,17 @@
+package com.dhsrocha.kata.tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application entry-point.
+ *
+ * @author <a href="mailto:dhsrocha.dev@gmail.com">Diego Rocha</a>
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
