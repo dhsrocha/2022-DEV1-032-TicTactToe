6078968efe552a7d5e785c0a92c7d457b1590cf3
Diego Henrique da Silva Rocha <dhsrocha.dev@gmail.com>
2022-06-28 11:53:56 +0200

    Dynamic OpenAPI info from injected Maven properties.

--- src/main/java/com/dhsrocha/kata/tictactoe/Application.java
+++ src/main/java/com/dhsrocha/kata/tictactoe/Application.java
@@ -1,7 +1,13 @@
 package com.dhsrocha.kata.tictactoe;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Application entry-point.
@@ -13,5 +19,32 @@ public class Application {
 
   public static void main(final String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @SuppressWarnings("unused")
+  @Configuration
+  static class DocumentationConfig {
+
+    @Value("${maven.project.name}")
+    private String name;
+
+    @Value("${maven.project.name}")
+    private String description;
+
+    @Value("${maven.project.version}")
+    private String version;
+
+    @Value("${maven.project.organization.name}")
+    private String organizationName;
+
+    @Value("${dhsrocha.author.email}")
+    private String email;
+
+    @Bean
+    OpenAPI openApi() {
+      final var contact = new Contact().name(organizationName).email(email);
+      return new OpenAPI()
+          .info(new Info().title(name).description(description).version(version).contact(contact));
+    }
   }
 }
