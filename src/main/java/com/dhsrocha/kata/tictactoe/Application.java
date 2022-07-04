603d5dbd715347b9adebb4fb619900b5b420bdb0
Diego Henrique da Silva Rocha <dhsrocha.dev@gmail.com>
2022-07-04 10:31:12 +0200

    Configs for OpenAPI's sort order.

--- src/main/java/com/dhsrocha/kata/tictactoe/Application.java
+++ src/main/java/com/dhsrocha/kata/tictactoe/Application.java
@@ -1,8 +1,14 @@
 package com.dhsrocha.kata.tictactoe;
 
+import com.dhsrocha.kata.tictactoe.feature.game.Game;
+import com.dhsrocha.kata.tictactoe.feature.player.Player;
+import com.dhsrocha.kata.tictactoe.feature.turn.Turn;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import java.util.Comparator;
+import java.util.List;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,26 +31,23 @@ public class Application {
   @Configuration
   static class DocumentationConfig {
 
-    @Value("${maven.project.name}")
-    private String name;
-
-    @Value("${maven.project.name}")
-    private String description;
-
-    @Value("${maven.project.version}")
-    private String version;
-
-    @Value("${maven.project.organization.name}")
-    private String organizationName;
-
-    @Value("${dhsrocha.author.email}")
-    private String email;
+    private static final List<String> TAGS = List.of(Player.TAG, Game.TAG, Turn.TAG);
 
     @Bean
-    OpenAPI openApi() {
-      final var contact = new Contact().name(organizationName).email(email);
+    OpenAPI openApi(
+        final @Value("${maven.project.name}") String name,
+        final @Value("${maven.project.name}") String description,
+        final @Value("${maven.project.version}") String version,
+        final @Value("${maven.project.organization.name}") String organization,
+        final @Value("${dhsrocha.author.email}") String email) {
+      final var contact = new Contact().name(organization).email(email);
       return new OpenAPI()
           .info(new Info().title(name).description(description).version(version).contact(contact));
+    }
+
+    @Bean
+    OpenApiCustomiser sortTags() {
+      return api -> api.getTags().sort(Comparator.comparingInt(t -> TAGS.indexOf(t.getName())));
     }
   }
 }
