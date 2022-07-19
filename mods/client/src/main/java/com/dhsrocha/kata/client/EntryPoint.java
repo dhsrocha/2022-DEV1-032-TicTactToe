d3e96199aeb902223c2856ba414eca32e3656484
Diego Henrique da Silva Rocha <dhsrocha.dev@gmail.com>
2022-07-19 19:07:51 +0200

    Created Client module.

---
+++ mods/client/src/main/java/com/dhsrocha/kata/client/EntryPoint.java
@@ -0,0 +1,14 @@
+package com.dhsrocha.kata.client;
+
+/**
+ * Application entry-point.
+ *
+ * @author <a href="mailto:dhsrocha.dev@gmail.com">Diego Rocha</a>
+ */
+public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
+
+  @Override
+  public void onModuleLoad() {
+
+  }
+}
